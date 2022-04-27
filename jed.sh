f81a5c528f9482e6cbbed0eae8001e4825d591c7
Sphinx <104243528+LOLsphinx@users.noreply.github.com>
2022-04-27 03:14:56 -0400

    jed.sh

--- jed.sh
+++ jed.sh
@@ -3,12 +3,6 @@ clear
 
 ##Hi I'm Jed AKA LOLsphinx!!!
 
-echo "My name is JED I made this kind of software!!!!!!"
-sleep 3
-
-echo "You can visit my facebook account: https://www.facebook.com/jed.villanueva.330/"
-
-sleep 5
 
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
@@ -16,6 +10,16 @@ MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\0
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
 RESETBG="$(printf '\e[0m\n')"
+
+
+
+echo "${MAGENTA}${WHITEBG}My name is JED I made this kind of software!!!!!!${RESETBG}"
+sleep 3
+
+echo "${RESETBG}${ORANGE}You can visit my facebook account:${RESETBG}${WHITEBG}${MAGENTA} https://www.facebook.com/jed.villanueva.330/${RESETBG}"
+
+sleep 5
+
 
 ## Directories
 if [[ ! -d ".server" ]]; then
